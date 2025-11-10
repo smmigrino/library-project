88c342bd6d9a11d4aab020f5916a740cc06f820d
smmigrino <smmigrino@student.apc.edu.ph>
2025-11-10 22:59:20 +0800

    added edit_book()

--- library_projtest_v1.py
+++ library_projtest_v1.py
@@ -139,7 +139,74 @@ def search():
             else:
                 print("Enter valid input. Try again.")
                 continue
+
+def edit_by_id():
+    while True:
+        id_search = input("Enter ID: ").strip()
+         
+        try:
+            id_search = int(id_search)
+            while True:
+                new_title = input("Book title: ").strip().upper()
+                if new_title:
+                    break
+                print("Please enter title.")
+                
+            while True:    
+                new_author = input("Author: ").strip().upper()
+                if new_author:
+                    break
+                print("Please enter author name.")
+            
+            while True:
+
+                try:
+                    new_year = input("Publication year (Type 0 for unknown year): ").strip()    
+                    new_year = int(new_year)
+                    break
+                
+                except ValueError:
+                    print("Please enter valid year or 0 if year is unknown.")
+                            
+
+            #duplicate handling portion    
+            cur.execute('SELECT COUNT(*) FROM  books WHERE title = ? AND author = ? AND id != ?', (new_title, new_author, id_search))
+            count = cur.fetchone()[0]
+            
+
+            if count == 0:
+                
+                cur.execute("""
+                        UPDATE books SET title = ?, author = ?, year = ? WHERE id = ?""", (new_title, new_author, new_year, id_search))
+                print("\n\nBook successfully updated.")
+                conn.commit()
+                break
+            else:
+                print("\nBook already in shelves. Try again.")
+                        
+                       
+        except ValueError:
+            print("Enter valid ID number. Try again.")
+            continue
+
+
+def edit_book():     
         
+    while True:
+        user_input = input("Do you know the ID? (y/n)").strip().lower()
+        
+        if user_input == 'n':
+            search()
+            edit_by_id()
+            break
+
+            
+        elif user_input == 'y':
+            edit_by_id()
+            break
+            
+        else:
+            print("Enter 'y' for yes, or 'n'  for no. Try again.")    
 
 
     
@@ -168,7 +235,7 @@ def main_menu():
             search()
             
         elif choice == 4:
-            print("Edit functions still in development hehehehe")
+            edit_book()
             
         elif choice == 5:
             print("Delete functions still in development hehehehe")
